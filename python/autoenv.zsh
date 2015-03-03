b59be70747013fce6dc9e3d700564a1d965e75a4
Michael Allen <michael@michaelallen.io>
2015-03-03 19:08:04 +0000

    activate autoenv when zsh auto cd

--- python/autoenv.zsh
+++ python/autoenv.zsh
@@ -1,5 +1,13 @@
+autoload -U add-zsh-hook
+
 autoenv="$(brew list autoenv)"
+
+autoenv_init_hook() {
+  autoenv_init
+}
 
 if [[ -n "$autoenv" ]]; then
   source /usr/local/opt/autoenv/activate.sh
 fi
+
+add-zsh-hook chpwd autoenv_init_hook
