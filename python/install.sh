5043414af886219691a77264d6b0821f0528afdf
Michael Allen <michael@michaelallen.io>
2015-04-09 11:30:54 +0100

    virtualenvwrapper to make activating virtualenvs easier

--- python/install.sh
+++ python/install.sh
@@ -1,5 +1,9 @@
 pip3 install pep8
 pip3 install flake8
+pip3 install virtualenv
+pip3 install virtualenvwrapper
 
 pip install pep8
 pip install flake8
+pip3 install virtualenv
+pip3 install virtualenvwrapper
---
+++ python/virtualenv.zsh
@@ -0,0 +1,4 @@
+export WORKON_HOME=$HOME/.virtualenvs
+export PROJECT_HOME=$HOME/Projects
+export VIRTUALENVWRAPPER_PYTHON=/usr/local/bin/python3
+source /usr/local/bin/virtualenvwrapper.sh
